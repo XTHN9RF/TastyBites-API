d0deefde50599910f10422685d04003ac9562004
Vladyslav Duran <justez4rtz@gmail.com>
2023-07-06 18:54:02 +0300

    feat: create user model and set it to be default

--- TastyBites_API/core/models.py
+++ TastyBites_API/core/models.py
@@ -1,3 +1,23 @@
-from django.db import models  # noqa: F401
+"""
+File that contains the models for the database.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    pass
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
