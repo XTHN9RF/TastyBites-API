f10be63b28067ec8531b58d7df32e0973269ce8c
Vladyslav Duran <justez4rtz@gmail.com>
2023-07-11 17:08:09 +0300

    feat: add user auth functionallity and refresh account feature

--- TastyBites_API/user/urls.py
+++ TastyBites_API/user/urls.py
@@ -4,10 +4,12 @@ URL configuration for user app
 
 from django.urls import path
 
-from .views import UserCreateView
+from .views import *
 
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', UserCreateView.as_view(), name='create_user'),
+    path('create/', RegistrationApiView.as_view(), name='create_user'),
+    path('login/', LoginApiView.as_view(), name='login_user'),
+    path('refresh/', RefreshApiView.as_view(), name='refresh_token'),
 ]
--- TastyBites_API/user/views.py
+++ TastyBites_API/user/views.py
@@ -2,11 +2,91 @@
 File contains all the views for the user app
 """
 
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
-from .serializer import UserSerializer
+from . import serializer
+from . import authentication
 
 
-class UserCreateView(generics.CreateAPIView):
-    """ View that helps to create new user """
-    serializer_class = UserSerializer
+class RegistrationApiView(APIView):
+    """Handle creating user accounts"""
+    serializer_class = serializer.UserSerializer
+
+    def post(self, request):
+        """Handle creating user accounts by checking if the data is valid and returning an access and refresh token"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            user = get_user_model().objects.get(email=request.data['email'])
+
+            response = Response()
+
+            access_token = authentication.create_access_token(user.id)
+            refresh_token = authentication.create_refresh_token(user.id)
+
+            response.set_cookie(key="refresh_token", value=refresh_token, httponly=True)
+            response.data = {
+                'token': access_token,
+                'message': 'User created successfully'
+            }
+            response.status_code = status.HTTP_201_CREATED
+            return response
+
+        return Response({"errorMessage": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginApiView(APIView):
+    """Handle logging in user accounts"""
+
+    def post(self, request):
+        """Login a user by checking email and password and returning an access and refresh token"""
+        user = get_user_model().objects.get(email=request.data['email'])
+
+        if not user:
+            return Response({'errorMessage': 'User with such email not found'}, status=status.HTTP_403_FORBIDDEN)
+
+        if not user.check_password(request.data['password']):
+            return Response({'errorMessage': 'Wrong password'}, status=status.HTTP_403_FORBIDDEN)
+
+        access_token = authentication.create_access_token(user.id)
+        refresh_token = authentication.create_refresh_token(user.id)
+
+        response = Response()
+        response.set_cookie(key="refresh_token", value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token,
+            'message': 'User logged in successfully'
+        }
+        response.status_code = status.HTTP_200_OK
+        return response
+
+
+class RefreshApiView(APIView):
+    """Handle refreshing access token"""
+
+    def get(self, request):
+        """Refresh access token by checking validity of refresh token and user data, then returns a new access token"""
+        refresh_token = request.COOKIES.get('refresh_token')
+        if not refresh_token:
+            return Response({'errorMessage': 'No refresh token'}, status=status.HTTP_403_FORBIDDEN)
+
+        user_id = authentication.decode_refresh_token(refresh_token)
+        if not user_id:
+            return Response({'errorMessage': 'Invalid refresh token'}, status=status.HTTP_403_FORBIDDEN)
+
+        user = get_user_model().objects.get(id=user_id)
+        if not user:
+            return Response({'errorMessage': 'User not found'}, status=status.HTTP_403_FORBIDDEN)
+
+        access_token = authentication.create_access_token(user.id)
+        response = Response()
+        response.data = {
+            'token': access_token,
+            'message': 'Access token refreshed successfully'
+        }
+        response.status_code = status.HTTP_200_OK
+        return response
